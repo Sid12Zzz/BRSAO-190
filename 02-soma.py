d4b0207b893bc6172348ca39fa6fe1f5abdf7780
Sid12Zzz <sidneijunioranjos@gmail.com>
2025-07-23 22:00:02 -0300

    Teste entregas atv

---
+++ 02-soma.py
@@ -0,0 +1,4 @@
+n1 =
+n2 = 
+soma = n1 + n2
+print('A soma entre {n1} e {n2} é igual a: {soma} ')
---
+++ 04-preco_total.py
@@ -0,0 +1,5 @@
+nome_produto = "Cadeira infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+print(f"Nome do Produto: {nome_produto}")
